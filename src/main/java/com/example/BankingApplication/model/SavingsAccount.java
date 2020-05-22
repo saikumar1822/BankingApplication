06eec45479bab9bbed330b1bf0b9b41f1c0d4826
saikumar1822 <saikumaryaddalapoori1822@gmail.com>
2020-05-22 14:32:00 +0530

    intial commit

---
+++ src/main/java/com/example/BankingApplication/model/SavingsAccount.java
@@ -0,0 +1,56 @@
+package com.example.BankingApplication.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table
+public class SavingsAccount {
+
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    private Long id;
+	    private int accountNumber;
+	    private long accountBalance;
+	    @OneToMany(mappedBy = "savingsAccount", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	    @JsonIgnore
+	    private List<SavingsTransaction> savingsTransactionList;
+	    
+
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public int getAccountNumber() {
+			return accountNumber;
+		}
+		public void setAccountNumber(int accountNumber) {
+			this.accountNumber = accountNumber;
+		}
+		public long getAccountBalance() {
+			return accountBalance;
+		}
+		public void setAccountBalance(long accountBalance) {
+			this.accountBalance = accountBalance;
+		}
+		public List<SavingsTransaction> getSavingsTransactionList() {
+			return savingsTransactionList;
+		}
+		public void setSavingsTransactionList(List<SavingsTransaction> savingsTransactionList) {
+			this.savingsTransactionList = savingsTransactionList;
+		}
+	    
+		
+}
